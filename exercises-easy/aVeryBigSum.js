aa6f074c3d1951a45fca262e8f85695277bd5d8e
Filipe nascimento <filipi.silvanascimento@gmail.com>
2023-08-10 21:36:50 -0300

    Function aVeryBigSum using the 'reduce' method

--- exercises-easy/aVeryBigSum.js
+++ exercises-easy/aVeryBigSum.js
@@ -21,3 +21,12 @@ function aVeryBigSum(ar) {
     }
     return long;
 }
+
+//Using Reduce
+
+function aVeryBigSumRdc(ar) {
+    // return ar.reduce((arSum,arr)=>arSum+arr,0);
+    console.log(ar.reduce((arSum,arr)=>arSum+arr,0));
+}
+
+aVeryBigSumRdc([1000000001, 1000000002, 1000000003, 1000000004, 1000000005]);
